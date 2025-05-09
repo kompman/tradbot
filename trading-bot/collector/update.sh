4ac24e259f1eae28774069f36060f369f0e6de3e
kompman <kompman1@gmail.com>
2025-05-09 16:48:37 +0300

    Update update.sh

--- trading-bot/collector/update.sh
+++ trading-bot/collector/update.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+cd "$(dirname "$0")"
+
+echo "Updating tradbot…"
 git pull origin main
 go mod tidy
 go build -o tradbot .
-# если systemd:
-sudo systemctl restart tradbot
+
+echo "Restarting tradbot…"
+pkill tradbot
+nohup ./tradbot > bot.log 2>&1 &
+
+echo "Done."
